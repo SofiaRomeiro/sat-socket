9930bb749fb54b3573c79df8da216523b2e7b2fc
SofiaRomeiro <sofiaromeiro23@tecnico.ulisboa.pt>
2023-10-24 20:49:47 +0100

    change server code

--- server-side.c
+++ server-side.c
@@ -10,6 +10,7 @@
 int main() {
     int server_socket;
     struct sockaddr_in server_addr;
+	struct sockaddr_in client_addr;
 	socklen_t client_addr_len = sizeof(client_addr);
 
     // Create a UDP socket
