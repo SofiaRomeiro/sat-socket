eb49f4cf3711252535d19ee6496b0daf66c0c58c
SofiaRomeiro <sofiaromeiro23@tecnico.ulisboa.pt>
2023-10-24 21:44:33 +0100

    change server code, add rdp1 and rdp2 code

---
+++ client-side-rdp1.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+#define SERVER_IP "10.10.10.7"
+#define SERVER_PORT 12345
+#define CLIENT_IP 0
+#define CLIENT_PORT 0
+#define MIN_PACKET_SIZE 10 //bytes
+#define MAX_PACKET_SIZE 256 //bytes
+
+// Function to generate a random string of variable length
+char *generateRandomString() {
+    int length = rand() % (MAX_PACKET_SIZE - MIN_PACKET_SIZE + 1) + MIN_PACKET_SIZE;
+    
+    char *random_string = (char *)malloc(length + 1);  // +1 for the null terminator
+    if (random_string == NULL) {
+        perror("Memory allocation error");
+        exit(1);
+    }
+    
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    int charset_length = sizeof(charset) - 1;
+    
+    for (int i = 0; i < length; i++) {
+        random_string[i] = charset[rand() % charset_length];
+    }
+    
+    random_string[length] = '\0';  // Null-terminate the string
+    return random_string;
+}
+
+int main() {
+
+    int client_socket;
+    struct sockaddr_in server_addr;
+    time_t t_send;
+    time_t t_recv;
+
+    srand(time(NULL));
+    
+//  --------------------------------- Create UDP socket -------------------------------------------------
+    client_socket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (client_socket == -1) {
+        perror("Error creating client socket");
+        exit(1);
+    }
+
+//  --------------------------------- Configure Server Address -------------------------------------------------
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+
+// -------------------------- Generate the packet to send --------------------------
+
+    while(1) {
+
+        char *packet = generateRandomString();
+        printf("Packet size: %u\n", sizeof(packet));
+
+        time(&t_send);
+
+        // Send data to the server
+        sendto(client_socket, packet, sizeof(packet), 0, (struct sockaddr*)&server_addr, sizeof(server_addr));
+
+        free(packet);  // Remember to free the allocated memory when done
+
+        // Receive a response from the server (optional)
+        char buffer[MAX_PACKET_SIZE];
+        memset(buffer, 0, sizeof(buffer));
+        int bytes_received = recvfrom(client_socket, buffer, sizeof(buffer), 0, NULL, NULL);
+        time(&t_recv);
+        if (bytes_received == -1) {
+            perror("Error receiving data");
+            exit(1);
+        }
+
+        printf("Received from server: %s\n", buffer);
+
+        double diff = difftime(t_recv, t_send);
+        printf("Tx: %.2f seconds\n", diff);
+
+        sleep(3);
+
+    }
+    
+    // Close the socket
+    close(client_socket);
+
+    return 0;
+}
--- server-side.c
+++ server-side.c
@@ -1,56 +1,161 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
 
 #define SERVER_IP "10.10.10.7"
+#define SERVER_PORT_RDP1 12345
+#define SERVER_PORT_RDP2 12346
+#define LINK_SPEED 1200 //bit/s
+#define MAX_BUFFER_SIZE 2049 //max packet size is 2048
+#define DELAYED_INTERFACE "eth1" 
+#define ADD 1
+#define DEL 0
 
-int main() {
-    int server_socket;
-    struct sockaddr_in server_addr;
-	struct sockaddr_in client_addr;
-	socklen_t client_addr_len = sizeof(client_addr);
+float delay = 0;
 
-    // Create a UDP socket
-    server_socket = socket(AF_INET, SOCK_DGRAM, 0);
-    if (server_socket == -1) {
+// This function is responsible for, given a packet size, computing the delay (for links with speed=1200 bit/s)
+// WARNING: the packet size is given in bytes so a conversion to bits is needed
+float link_delay_tx(int packet_size) {
+	return (packet_size * 8)/LINK_SPEED;
+}
+
+int run_command(float delay, int action) {
+
+	FILE *fp;
+	char path[1035];
+
+	char* command = "";
+	if (action) {
+		printf("tc qdisc add dev %s root netem delay %fms", DELAYED_INTERFACE, delay, command);
+	}
+	else {
+		printf("tc qdisc del dev %s root netem delay %fms", DELAYED_INTERFACE, delay, command);
+	}  
+
+	/* Open the command for reading. */
+	fp = popen(command, "r");
+	if (fp == NULL) {
+		printf("Failed to run command\n" );
+		exit(1);
+	}
+
+	printf("Output:\n")
+  /* Read the output a line at a time - output it. */
+	while (fgets(path, sizeof(path), fp) != NULL) {
+		printf("%s", path);
+	}
+
+  /* close */
+	pclose(fp);
+
+  	return 0;
+}
+
+void link_socket() {
+	int server_socket_rdp1;
+	int server_socket_rdp2;
+    struct sockaddr_in server_addr_rdp1;
+	struct sockaddr_in server_addr_rdp2;
+	struct sockaddr_in client_addr_rdp1;
+	struct sockaddr_in client_addr_rdp2;
+	socklen_t client_addr_rdp1_len = sizeof(client_addr_rdp1);
+	socklen_t client_addr_rdp2_len = sizeof(client_addr_rdp2);
+
+//  --------------------------------- Create UDP sockets -------------------------------------------------
+    server_socket_rdp1 = socket(AF_INET, SOCK_DGRAM, 0);
+    if (server_socket_rdp1 == -1) {
         perror("Error creating server socket");
         exit(1);
     }
 
-    // Configure server address
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(12345);  // Port number
-    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);  // Replace with the interface IP
+	server_socket_rdp2 = socket(AF_INET, SOCK_DGRAM, 0);
+    if (server_socket_rdp2 == -1) {
+        perror("Error creating server socket");
+        exit(1);
+    }
+
+//  --------------------------------- Configure Server Addresses -------------------------------------------------
+
+    server_addr_rdp1.sin_family = AF_INET;
+    server_addr_rdp1.sin_port = htons(SERVER_PORT_RDP1);
+    server_addr_rdp1.sin_addr.s_addr = inet_addr(SERVER_IP); 
+
+	server_addr_rdp2.sin_family = AF_INET;
+    server_addr_rdp2.sin_port = htons(SERVER_PORT);
+    server_addr_rdp2.sin_addr.s_addr = inet_addr(SERVER_IP); 
+
+//  --------------------------------- Bind Server Sockets -------------------------------------------------
+
 
     // Bind the socket to the specific interface address
-    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+    if (bind(server_socket_rdp1, (struct sockaddr*)&server_addr_rdp1, sizeof(server_addr_rdp1)) == -1) {
         perror("Error binding server socket");
         exit(1);
     }
 
-    // Rest of the server code remains the same
-	char buffer[1024];
+	    // Bind the socket to the specific interface address
+    if (bind(server_socket_rdp2, (struct sockaddr*)&server_addr_rdp2, sizeof(server_addr_rdp2)) == -1) {
+        perror("Error binding server socket");
+        exit(1);
+    }
+
+//  --------------------------------- Receive from RDP1 and RDP2 -------------------------------------------------
+
+	char buffer_rdp1[MAX_BUFFER_SIZE];
+	char buffer_rdp2[MAX_BUFFER_SIZE];
     
     while (1) {
-        // Receive data from the client
-        memset(buffer, 0, sizeof(buffer));
-        int bytes_received = recvfrom(server_socket, buffer, sizeof(buffer), 0, (struct sockaddr*)&client_addr, &client_addr_len);
+        // Receive data from rdp1
+        memset(buffer_rdp1, 0, sizeof(buffer_rdp1));
+        int bytes_received = recvfrom(server_socket_rdp1, buffer_rdp1, sizeof(buffer_rdp1), 0, (struct sockaddr*)&client_addr_rdp1, &client_addr_rdp1_len);
         if (bytes_received == -1) {
             perror("Error receiving data");
             exit(1);
         }
 
-        printf("Received: %s\n", buffer);
+        printf("Received from RDP1: %s\n", buffer_rdp1);
 
-        // Respond to the client (optional)
-        sendto(server_socket, "Hello, client!", 13, 0, (struct sockaddr*)&client_addr, client_addr_len);
+		// Process delay and call netemu
+
+		if (delay != 0) {
+			run_command(delay, DEL);
+		}
+
+		delay = link_delay_tx(bytes_received);
+
+		run_command(delay, ADD);
+
+        // Forward to RDP2
+        sendto(server_socket_rdp2, buffer_rdp1, 13, 0, (struct sockaddr*)&client_addr_rdp2, client_addr_rdp2_len);
+
+
+		// Wait for RDP2 answer
+		memset(buffer_rdp2, 0, sizeof(buffer_rdp2));
+        int bytes_received = recvfrom(server_socket_rdp2, buffer_rdp2, sizeof(buffer_rdp2), 0, (struct sockaddr*)&client_addr_rdp2, &client_addr_rdp2_len);
+        if (bytes_received == -1) {
+            perror("Error receiving data");
+            exit(1);
+        }
+
+		printf("Received from RDP2: %s\n", buffer_rdp2);
+
+		// Forward to RDP1
+        sendto(server_socket_rdp1, buffer_rdp2, 13, 0, (struct sockaddr*)&client_addr_rdp1, client_addr_rdp1_len);
+
+
     }
 
     // Close the socket
-    close(server_socket);
+    close(server_socket_rdp1);
+	close(server_socket_rdp2);
 
     return 0;
+}
+
+int main() {
+
 }
