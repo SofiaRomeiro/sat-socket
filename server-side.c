f9f794ed3ef003fd4f12e0c61f4e096b38e59daa
SofiaRomeiro <sofiaromeiro23@tecnico.ulisboa.pt>
2023-10-24 20:47:30 +0100

    change server code

--- server-side.c
+++ server-side.c
@@ -2,19 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
 
 #define SERVER_IP "10.10.10.7"
-#define SERVER_PORT 12345
-#define CLIENT_IP 
-#define CLIENT_PORT
 
 int main() {
     int server_socket;
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t client_addr_len = sizeof(client_addr);
-    
-    // Create a UDP socket
-    server_socket = socket(AF_INET, SOCK_DGRAM, 0);
+    struct sockaddr_in server_addr;
+
+    // Create a socket
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (server_socket == -1) {
         perror("Error creating server socket");
         exit(1);
@@ -22,16 +20,17 @@ int main() {
 
     // Configure server address
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(SERVER_PORT);  // Port number
-    server_addr.sin_addr.s_addr = SERVER_IP;
+    server_addr.sin_port = htons(12345);  // Port number
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);  // Replace with the interface IP
 
-    // Bind the socket to the server address
+    // Bind the socket to the specific interface address
     if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
         perror("Error binding server socket");
         exit(1);
     }
 
-    char buffer[1024];
+    // Rest of the server code remains the same
+	char buffer[1024];
     
     while (1) {
         // Receive data from the client
